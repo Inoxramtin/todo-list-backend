e69d710271417d0b8013bc0658995b9a4575b862
Ramtin Pakzadan <ramtin.pakzadan379@gmail.com>
2024-08-21 20:55:32 +0330

    database handler

---
+++ src/core/database/database-handler.js
@@ -0,0 +1,26 @@
+import pg from 'pg'
+import { POSTGRES_CREDENTIALS } from '../secrets/index.js';
+
+const { Client } = pg;
+
+const query = async (query , params) =>{
+    let client;
+    try {
+        console.log("query try")
+        client = new Client(POSTGRES_CREDENTIALS);
+        await client.connect();
+        const res = await client.query(query, params)
+        return res;
+     } catch (err) {
+        console.error(err);
+        return null;
+     } finally {
+        console.log("query finally")
+        await client.end()
+     }
+     
+}
+
+export{
+    query
+};
